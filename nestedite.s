87abda315f952e1e84e39795b209869afcd17950
cchandrashekhar104 <cchandrashekhar104@gmail.com>
2017-12-02 00:38:46 +0530

    Update nestedite.s

--- nestedite.s
+++ nestedite.s
@@ -7,14 +7,14 @@ __main  FUNCTION
 	MOV r2,#5
 	MOV r4,#3
 	MOV r3,#0
-;loop CMP r2,r4
+
 	CMP r2,r4
 	ITTEE GT
 	SUBGT r2,r2,r4
 	ADDGT r3,#1
 	SUBLE r4,r4,r2
 	ADDLE r3,#2
-	;BNE loop
+
 stop B stop ; stop program
      ENDFUNC
      END
