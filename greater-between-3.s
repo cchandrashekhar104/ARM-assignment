ba351d4af6f4bf6184a168744c7fcdca85af70b9
cchandrashekhar104 <cchandrashekhar104@gmail.com>
2017-12-02 00:11:18 +0530

    Update greater-between-3.s

--- greater-between-3.s
+++ greater-between-3.s
@@ -1,20 +1,21 @@
-  PRESERVE8
+     PRESERVE8
      THUMB
- AREA     appcode, CODE, READONLY
+     AREA     appcode, CODE, READONLY
      EXPORT __main
 	 ENTRY 
-__main  FUNCTION
-	MOV r2,#6
-	MOV r4,#4
-	MOV r5,#8
-	MOV r6,#0
-	MOV r1,#0x20000000
-	CMP r2,r4
-	STRLT r4,[r1]
-	STRGT r2,[r1]
-	LDR r6,[r1]
-	CMP r6,r5
-	MOV r6,r5
+__main  FUNCTION	
+		/*Get the largest number between 3 numbers
+		The result will be stored in R3
+		*/
+		MOV R0,#8	;number 1
+		MOV R1,#6	;number 2
+		MOV R2,#9	;number 3
+		MOV R3,R0	;if r0 == r1
+		CMP R0,R1	;compare r0 and r1
+		MOVLT R3,R1	;if r1 is greater move it to r3
+		MOVGT R3,R0	;if r0 is greater move it to r3
+		CMP R3,R2	;finally compare r3 and r2
+		MOVLT R3,R2	;if r2 is greater move it to r3
 stop B stop ; stop program
      ENDFUNC
      END
