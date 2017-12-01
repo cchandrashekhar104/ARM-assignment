8cf9a6c89c28cb16aeb06a54df3eb4ce8035d6b6
cchandrashekhar104 <cchandrashekhar104@gmail.com>
2017-12-02 00:27:00 +0530

    Update gcd.s

--- gcd.s
+++ gcd.s
@@ -1,17 +1,29 @@
-  PRESERVE8
+     PRESERVE8
      THUMB
- AREA     appcode, CODE, READONLY
+     AREA     appcode, CODE, READONLY
      EXPORT __main
 	 ENTRY 
-__main  FUNCTION
-	MOV r2,#16
-	MOV r4,#8
-loop CMP r2,r4
-	CMP r2,r4
-	ITE GT
-	SUBGT r2,r2,r4
-	SUBLE r4,r4,r2
-	BNE loop
+__main  FUNCTION	
+		/*
+		Calculating gcd using the following algorithm
+		while (a != b)
+		{
+			if (a > b)
+				a = a – b;
+			else
+				b = b – a;
+		}
+		a=R0
+		b=R1
+		*/
+		MOV R0,#100
+		MOV R1,#16
+while	CMP R0,R1	;compare r0,r1
+		BEQ stop	;if equal we got the gcd come out of the while loop
+		IT GT		
+		SUBGT R0,R0,R1	;if r0>r1 r0=r0-r1 
+		SUBLT R1,R1,R0	;if r1>r0 r1=r1-r0
+		B while
 stop B stop ; stop program
      ENDFUNC
      END
