8224caff9556c8605eef3cf9a866a380fa015b2e
cchandrashekhar104 <cchandrashekhar104@gmail.com>
2017-12-02 09:53:22 +0530

    Update nestedite.s

--- nestedite.s
+++ nestedite.s
@@ -1,20 +1,33 @@
-  PRESERVE8
+     PRESERVE8
      THUMB
- AREA     appcode, CODE, READONLY
+     AREA     appcode, CODE, READONLY
+	 GBLA x		;define global arithmetic variable x
+x 	 SETA 9		;set x as 9
+	 GBLA y		;define global arithmetic variable y
+y 	 SETA 2		;set y as 2
+	 GBLA z		;define global arithmetic variable z
+z 	 SETA 37	;set z as 37
+	 GBLA w		;define global variable w
+w 	 SETA 19	;set w as 19
+		 		
      EXPORT __main
 	 ENTRY 
-__main  FUNCTION
-	MOV r2,#5
-	MOV r4,#3
-	MOV r3,#0
+__main  FUNCTION	
+		MOV R0,#0	;R0 will get value based on nested if else condition
 
-	CMP r2,r4
-	ITTEE GT
-	SUBGT r2,r2,r4
-	ADDGT r3,#1
-	SUBLE r4,r4,r2
-	ADDLE r3,#2
-
+		IF x>y
+			IF z>w
+				MOV R0,#1
+			ELSE
+				MOV R0,#2
+			ENDIF
+		ELSE
+			IF z>w
+				MOV R0,#3
+			ELSE
+				MOV R0,#4
+			ENDIF
+		ENDIF
 stop B stop ; stop program
      ENDFUNC
      END
